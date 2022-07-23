40b5c272b5551f670c003f80f71492ca45bee6fa
Ramfile$ <54764417+ramfiles@users.noreply.github.com>
2022-07-23 14:37:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -67,53 +67,7 @@ require "product-data.php"; //PRODUCTS
 
     </div>
 
-    <script>
-
-        function addBasket(id)
-        {
-            $.ajax({
-                type: "POST",
-                url: "addBasket.php",
-                data: {"id": id},
-                dataType: "json",
-                success: function(response){
-                }
-            });
-            $(this).html("Edit");
-        }
-
-        $(document).ready(function(){
-           $(".basketBtn").click(function(e){
-              if ($(this).hasClass("btn btn-success text-white basketBtn"))
-              {
-                  $(this).removeClass("btn btn-success text-white basketBtn");
-                  $(this).addClass("btn btn-danger text-white basketBtn");
-                  $(this).html("remove from Basket");
-              } else {
-                  $(this).removeClass("btn btn-danger text-white basketBtn");
-                  $(this).addClass("btn btn-success text-white basketBtn");
-                  $(this).html("add to Basket");
-              }
-           });
-        });
-
-        function getData()
-        {
-            $.ajax({
-                type: "POST",
-                url: "getData.php",
-                success: function(e){
-                    if (!e){
-                        e = 0;
-                    }
-                    $(".shoppingCartNumber").html("(" + e + ")");
-                }
-            });
-        }
-
-        setInterval("getData()", 1000);
-
-    </script>
+    <script src="action.js"></script>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
